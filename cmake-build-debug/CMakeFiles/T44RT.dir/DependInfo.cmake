b2ac3f24697a02d0241a677e136d108b2177a892
Yurij Zaytsev <77232884+Hello-zoka@users.noreply.github.com>
2021-08-25 00:51:45 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/T44RT.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yurij/Documents/zoka/Прога/T44RT/Ray_tracing_main.cpp" "/Users/yurij/Documents/zoka/Прога/T44RT/cmake-build-debug/CMakeFiles/T44RT.dir/Ray_tracing_main.cpp.o"
+  "/Users/yurij/Documents/zoka/Прога/T44RT/materials.cpp" "/Users/yurij/Documents/zoka/Прога/T44RT/cmake-build-debug/CMakeFiles/T44RT.dir/materials.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
